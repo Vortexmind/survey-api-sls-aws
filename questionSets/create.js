5b1b7b03d1b5b1fbeea7ecd6b856e22893f16f15
Paolo Tagliaferri <vortexmind@gmail.com>
2017-02-12 01:46:54 +0000

    Include validation for question sets and avoid updating existing entries

--- questionSets/create.js
+++ questionSets/create.js
@@ -4,23 +4,38 @@ const AWS = require('aws-sdk');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+var validateQuestions = function(questionList){
+
+  if (!(Array.isArray(questionList))) return false;
+
+  for (var i=0; i < questionList.length; i++) {
+    if (typeof questionList[i].question !== 'string' ||
+        !(Array.isArray(questionList[i].answers)) ) return false;
+    for (var j=0; j < questionList[i].answers.length; j++) {
+      if (typeof questionList[i].answers[j] !== 'string') return false;
+    }
+  }
+  return true;
+}
+
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
-  if (typeof data.questions !== 'string' || typeof data.id  !== 'number') {
+  if (typeof data.surveyId  !== 'number' || !(validateQuestions(data.questions)) ) {
     console.error('Validation Failed');
-    callback(new Error('Could not create survey'));
+    callback(new Error('Could not create question set'));
     return;
   }
 
   const params = {
     TableName: process.env.QUESTION_SETS_TABLE,
     Item: {
-      id: data.id,
+      surveyId: data.surveyId,
       MapAttribute: data.questions,
       createdAt: timestamp,
       updatedAt: timestamp,
     },
+    ConditionExpression: 'attribute_not_exists(surveyId)'
   };
 
   dynamoDb.put(params, (error, result) => {
